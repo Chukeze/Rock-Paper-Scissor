5ba03dfc72b7b33dbf86e8ea8fcb80907a64affb
Chukeze <enile12@gmail.com>
2022-11-03 22:29:52 -0500

    Create Variables and Functions
    markup the html. Create the functions for getting inputs
    There will be two inputs user inputs and ai input.
    Gave rock, paper, and scissor int so when we compare user choices
    there won't be any need to convert string to input.
    
    this is the initial structure design, as I refactor I will most  fixed
    the logic so rather than predefined numbers, it more dynamic, might use
    an array but first wanted to get the basic functionality of selecting
    and comparing choices to see winner working first.

---
+++ assests/scripts/script.js
@@ -0,0 +1,28 @@
+//Ui yes but later
+
+//Input:  User input and Ai inputs
+
+//Output : Decide Winner
+
+//Psuedocode 
+/**
+ * For every user choice
+ * Ai makes a choice
+ * compare user choice to ai chose
+ * which ever has higher precedence wins
+ */
+
+const rock = 1;
+const paper = 2;
+const scissor = 3;
+
+const getComputerChoice = () => {
+
+}
+const getUserChoice = () => {
+
+}
+
+(computerChoice, userChoice) => {
+    return;
+}
