615054903cff45ebc6eb1a53b311983bba0bdfbd
Chukeze <enile12@gmail.com>
2022-11-09 23:33:15 -0600

    Refactor Core Logic

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -12,17 +12,48 @@
  * which ever has higher precedence wins
  */
 
-const rock = 1;
-const paper = 2;
-const scissor = 3;
 
-const getComputerChoice = () => {
+const options = ['Rock', 'Paper', 'Scissors'];
 
+const getComputerChoice = (options) => {
+    //Return a Random choice out of the options
+    return console.log(options[Math.floor(Math.random() * options.length)]);
 }
 const getUserChoice = () => {
-
+    const question = prompt("Select Rock, Paper, or Scissor");
+    console.log(question);
+    return question;
 }
 
-(computerChoice, userChoice) => {
-    return;
+function decision(computerChoice, userChoice) {
+    if(computerChoice === userChoice){
+        return "Tie, Try Again";
+    }
+
+    /*if(computerChoice == userChoice){
+        return "Tie, Try Again";
+    }else if((computerChoice !== userChoice) && (((userChoice || computerChoice) == "Rock") && ((userChoice || computerChoice) != "Paper")) ){
+        return  `${(computerChoice == "Rock" && userChoice == "Scissor") ? "You Lose Computer Win, Rock beats scissors" : "You Win Computer Loses"}`;
+    }else if((computerChoice !== userChoice) && (((userChoice || computerChoice) == "Paper") && ((userChoice || computerChoice) != "Scissor" ))){
+        return `${(computerChoice == "Paper" && userChoice == "Rock") ? "You Lose Computer Win, Paper beats rock" : "You Win Computer Loses"}`;
+    }else if((computerChoice !== userChoice) && (((userChoice || computerChoice) == "Scissor") && ((userChoice || computerChoice) != "Rock" ))){
+        return `${(computerChoice == "Scissor" && userChoice == "Paper") ? "You Lose Computer Win, Scissor beats paper" : "You Win Computer Loses"}`;
+    }else{
+        return "You Lose";
+    }*/
 }
+
+const run = () => {
+    for(let i = 0; i < 5; i++){
+        console.log(decision(getComputerChoice(options),getUserChoice()));
+    }
+    
+    //const start = document.createElement('button');
+    //start.textContent = "StartGame";
+    //document.body.appendChild(start);
+    //start.addEventListener('click', decision());
+}
+
+run();
+
+
