8fa8dae14aec6ae8f898d4c9e3bc1db27c0eabd4
Chukeze <enile12@gmail.com>
2022-11-18 23:30:09 -0600

    Fix UI Logic
    This will make the user score change when the button are clicked.
    
    The error was occurring because the conditional statements in The
    playground always evaluated to false because the character were never
    lowercase in either the array or the button values. and check score
    always returned a string rather than a number.

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -5,9 +5,12 @@
  * compare user choice to ai chose
  * which ever has higher precedence wins
  */
+
 const options = ['Rock', 'Paper', 'Scissors'];
 let computerScore = 3;
 let userScore = 3;
+let userChoice;
+
 
 
 //UI
@@ -17,21 +20,36 @@ function UI () {
     const container = document.createElement('main');
     const optionContainer = document.createElement('div');
     const scoreBoxContainer = document.createElement('div');
-    
+    const declareWinner = document.createElement('p');
+    const userScoreContainer = document.createElement('div');
+    const computerScoreContainer = document.createElement('div');
+    const userScoreIdentifierContainer = document.createElement('p');
+    const userScoreIdentifier = document.createElement('b');
+    const computerScoreIdentifierContainer = document.createElement('p');
+    const computerScoreIdentifier = document.createElement('b');
 
     container.classList.add("canvas")
     optionContainer.classList.add("option-container");
     scoreBoxContainer.classList.add("scorebox-container");
+    declareWinner.classList.add("winner-declaration");
 
     body.appendChild(container);
     container.appendChild(header);
     container.appendChild(optionContainer);
     container.appendChild(scoreBoxContainer);
-    
-    
+    scoreBoxContainer.appendChild(userScoreContainer);
+    scoreBoxContainer.appendChild(computerScoreContainer);
+    userScoreContainer.appendChild(userScoreIdentifierContainer);
+    userScoreIdentifierContainer.appendChild(userScoreIdentifier);
+    computerScoreContainer.appendChild(computerScoreIdentifierContainer);
+    computerScoreIdentifierContainer.appendChild(computerScoreIdentifier);
+
+
+    userScoreIdentifier.textContent = 'User';
+    computerScoreIdentifier.textContent = 'Cpu';
 
     for (let i = 0; i < options.length; i++) {
-        const element = document.createElement('option');
+        const element = document.createElement('button');
         element.value = options[i];
         //element.textContent = options[i];
         optionContainer.appendChild(element);
@@ -45,16 +63,16 @@ function UI () {
             const element = document.createElement('p');
             element.textContent = userScore;
             element.id = 'user-score';
-            scoreBoxContainer.appendChild(element);
+            userScoreContainer.appendChild(element);
         }
         if(index == 2){
             const element = document.createElement('p');
             element.textContent = computerScore;
             element.id='computer-score';
-            scoreBoxContainer.appendChild(element);
+            computerScoreContainer.appendChild(element);
         }
     }
-    const option = document.querySelectorAll('option');
+    const option = document.querySelectorAll('button');
     const optionArray = Array.from(option);
     const imageOption = ['./assests/images/rock.png', './assests/images/paper.png', './assests/images/scissors.png'];
     for (let index = 0; index < imageOption.length; index++) {
@@ -71,100 +89,209 @@ function UI () {
         //button.appendChild(image);
         });
     } 
-}
-    /*for (let index = 0; index < imageOption.length; index++) {
-    const getImages = document.querySelectorAll('img');
-    getImages.forEach(img => {
-        img.src = imageOption[index++];
-        img.alt = options;
-    });        
-    }  */  
 
-let listenForUserEvent = document.querySelectorAll('option');
-/*
-listenForUserEvent.forEach((button) =>{
-    button.addEventListener('click', () =>{
-        const userChoice = button.value.toString();
-        playRound(userChoice,getComputerChoice(options));
-    })
-});*/
+    scoreBoxContainer.insertBefore( declareWinner, computerScoreContainer);
+}
+UI();
+//Logic
 
 const getComputerChoice = (options) => {
     //Return a Random choice out of the options
     return options[Math.floor(Math.random() * options.length)];
 }
+const buttons  = document.querySelectorAll('button');
+buttons.forEach((button) =>{
+    button.addEventListener("click", () =>{
+        //console.log(typeof(button.value));
+        userChoice = button.value;
+        playRound(userChoice, getComputerChoice(options));
+    });
+});
 
-function playRound(userChoice, computerChoice) {
+
+
+function playRound(userChoice,computerChoice) {
     const score = checkScore();
     const getUserCurrentScore = document.getElementById('user-score');
+
     const getComputerCurrentScore = document.getElementById('computer-score');
     // for each user choice ai make choice 
-    if((computerChoice[0].toLowerCase() == userChoice[0].toLowerCase()) && (score != 0)){
+    if((computerChoice == userChoice) && (score != 0)){
         return " Tie, Try Again";
     }else if(
         (
-            (userChoice[0].toLowerCase() == "Rock" && computerChoice[0].toLowerCase() == "Scissors") || 
-            (userChoice[0].toLowerCase() == "Paper" && computerChoice[0].toLowerCase() == "Rock") || 
-            (userChoice[0].toLowerCase() == "Scissors" && computerChoice[0].toLowerCase() == "Paper")
+            (userChoice == "Rock" && computerChoice == "Scissors") || 
+            (userChoice == "Paper" && computerChoice == "Rock") || 
+            (userChoice == "Scissors" && computerChoice == "Paper")
         ) && (score != 0)
     ){
-        getUserCurrentScore.textContent = --computerScore;;
+        console.log(computerChoice);
+        console.log(userChoice);
+        computerScore = --computerScore;
+        getUserCurrentScore.textContent = computerScore;
+        checkScore(userScore,computerScore);
     }else if(
         (
-            (userChoice[0].toLowerCase() == "Rock" && computerChoice[0].toLowerCase() == "Paper") || 
-            (userChoice[0].toLowerCase() == "Paper" && computerChoice[0].toLowerCase() == "Scissors") || 
-            (userChoice[0].toLowerCase() == "Scissors" && computerChoice[0].toLowerCase() == "Rock")
+            (userChoice == "Rock" && computerChoice == "Paper") || 
+            (userChoice == "Paper" && computerChoice == "Scissors") || 
+            (userChoice == "Scissors" && computerChoice == "Rock")
         ) && (score != 0)
     ){
-        getComputerCurrentScore.textContent = --userScore;
-    }else{
-        return;
-    }
-}
-
-function checkScore () {
-    if(userScore == 0){
-        return "You Lose";
-    }else if(computerScore == 0){
-        return "You Win";
-    }else{
-        return;
-    }
-}
-
-function updateScore(roundResult){
-    const getUserCurrentScore = document.getElementById('user-score');
-    const getComputerCurrentScore = document.getElementById('computer-score');
-    if(roundResult === ` "You Won This Round, Computer Lose One Life, " + ${--computerScore} + " "`){
-        getUserCurrentScore.textContent = computerScore;
-    }else if(roundResult === `"Computer Won This Round, You Lose One Life, " + ${--userScore} + " "`){
+        console.log(computerChoice);
+        console.log(userChoice);
+        userScore = --userScore;
         getComputerCurrentScore.textContent = userScore;
+        checkScore(userScore,computerScore);
     }else{
         return;
     }
 }
-//console.log(` "You Won This Round, Computer Lose One Life, " + ${--computerScore} + " "`)
 
+function checkScore (userScore,computerScore) {
+    if(userScore == 0){
+        declareWinner("You Lose! Get back in the Gym and train some more", userScore);
+    }else if(computerScore == 0){
+        declareWinner("You Won! Keep going chase the world Championship", computerScore);
+    }else{
+        return;
+    }
+}
+
+function declareWinner(str='',score) {
+    let showWinner = document.querySelector('.winner-declaration');
+    if(score == 0){
+        showWinner.textContent = str;
+        
+    }
+}
+
+function showRoundResult(userChoice, computerChoice){
+    const modal = document.createElement('div');
+    let showChoiceMadeByUser;
+    let showChoiceMadeByCpu;
+    if(userChoice == "Rock" && computerChoice == "Scissors"){
+        showChoiceMadeByUser = document.createElement('div');
+        showChoiceMadeByCpu = document.createElement('div');
+        showChoiceMadeByUser.style.backgroundImage = `url()`;
+        showChoiceMadeByCpu.style.backgroundImage = `url("./assests/images/hand-scissors.png")`;
+        modal.appendChild(showChoiceMadeByUser);
+        modal.appendChild(showChoiceMadeByCpu);
+    }
+    if(userChoice == "Paper" && computerChoice == "Rock"){
+        showChoiceMadeByUser = document.createElement('div');
+        showChoiceMadeByCpu = document.createElement('div');
+        showChoiceMadeByUser.style.backgroundImage = `url("./assests/images/paper-hand")`;
+        showChoiceMadeByCpu.style.backgroundImage = `url("./assests/images/rock-reversed.png")`;
+        modal.appendChild(showChoiceMadeByUser);
+        modal.appendChild(showChoiceMadeByCpu);
+    }
+    if(userChoice == "Scissors" && computerChoice == "Paper"){
+        modal.style.backgroundImage = `url("./assests/images/scissors-beating-paper.png")`
+    }
+    if(userChoice == "Rock" && computerChoice == "Paper"){
+        showChoiceMadeByUser = document.createElement('div');
+        showChoiceMadeByCpu = document.createElement('div');
+        showChoiceMadeByUser.style.backgroundImage = `url("./assests/images/rock-hand.png")`;
+        showChoiceMadeByCpu.style.backgroundImage = `url("./assests/images/hand-reversed.png")`;
+        modal.appendChild(showChoiceMadeByUser);
+        modal.appendChild(showChoiceMadeByCpu);
+    }
+    if(userChoice == "Paper" && computerChoice == "Scissors"){
+        showChoiceMadeByUser = document.createElement('div');
+        showChoiceMadeByCpu = document.createElement('div');
+        showChoiceMadeByUser.style.backgroundImage = `url("./assests/images/paper-hand.png")`;
+        showChoiceMadeByCpu.style.backgroundImage = `url("./assests/images/hand-scissors.png")`;
+        modal.appendChild(showChoiceMadeByUser);
+        modal.appendChild(showChoiceMadeByCpu);
+    }
+    if(userChoice == "Scissors" && computerChoice == "Rock"){
+        showChoiceMadeByUser = document.createElement('div');
+        showChoiceMadeByCpu = document.createElement('div');
+        showChoiceMadeByUser.style.backgroundImage = `url("./assests/images/hand-scissors-reversed.png)`;
+        showChoiceMadeByCpu.style.backgroundImage = `url("./assests/images/rock-reversed.png")`;
+        modal.appendChild(showChoiceMadeByUser);
+        modal.appendChild(showChoiceMadeByCpu);
+    }
+    if(userChoice == "Rock" && computerChoice == "Rock"){
+        modal.style.backgroundImage = `url("./assests/images/rock-rock.png")`;
+    }
+    if(userChoice == "Paper" && computerChoice == "Paper"){
+        modal.style.backgroundImage = `url("./assests/images/paper-paper.png")`;
+    }
+    if(userChoice == "Scissors" && computerChoice == "Scissors"){
+        showChoiceMadeByUser = document.createElement('div');
+        showChoiceMadeByCpu = document.createElement('div');
+        showChoiceMadeByUser.style.backgroundImage = `url("./assests/images/hand-scissors-reversed.png)`;
+        showChoiceMadeByCpu.style.backgroundImage = `url("./assests/images/hand-scissors.png")`;
+        modal.appendChild(showChoiceMadeByUser);
+        modal.appendChild(showChoiceMadeByCpu);
+    }
+    if(userChoice == "Gun" && computerChoice == "Gun"){
+        showChoiceMadeByUser = document.createElement('div');
+        showChoiceMadeByCpu = document.createElement('div');
+        showChoiceMadeByUser.style.backgroundImage = `url("./assests/images/gun-hand.png)`;
+        showChoiceMadeByCpu.style.backgroundImage = `url("./assests/images/gun-hand.png")`;
+        modal.appendChild(showChoiceMadeByUser);
+        modal.appendChild(showChoiceMadeByCpu);
+    }
+}
+
+function storeGameResult(result) {
+    let gameresult = [];
+    return gameresult.push(result);
+}
+
+function createGun(){
+    const gunButton = document.createElement('button');
+    const findOptions = document.querySelector('.option-container');
+    gunButton.style.backgroundImage = `url("./assests/images/gun-hand.png")`;
+    findOptions.appendChild(gunButton);
+}
+
+function activateGun(gameresult){
+    if(gameresult == "You Won! Keep going chase the world Championship" ){
+        createGun();
+    }
+}
+
+/*const listenForUserEvent = document.querySelectorAll('button');
+const convertListenForUserEventNodeListIntoArrays = Array.from(listenForUserEvent);
+
+console.log(convertListenForUserEventNodeListIntoArrays)
+
+
+const getUserChoice = (choice) => {
+    return choice;
+    return choice.forEach(button =>{
+        button.addEventListener('click', () =>{
+            playRound(button.value, getComputerChoice(options))
+        });
+    });   
+}*/ 
+/*
+const storeUserChoice = (storedChoice) => {
+    return storedChoice;
+}*/
+
+
+
+
+
+
+
+/*
 function recordRoundResults (round) {
     const roundCount = [];
     for( const rounds of roundCount){
         rounds.push(round);
     }
     return roundCount;
-}
-const run = () => {
-    recordRoundResults(updateScore
-            (listenForUserEvent.forEach((button) =>{
-                    button.addEventListener('click', () =>{
-                        const userChoice = button.value.toString();
-                        playRound(userChoice,getComputerChoice(options));
-                    })
-                })
-            )
-        );   
-}
-
-for (let index = 0; index < 2; index++) {
-    run();
-}
-UI();
+}*/
+   
+/*for (let index = 0; index < imageOption.length; index++) {
+    const getImages = document.querySelectorAll('img');
+    getImages.forEach(img => {
+        img.src = imageOption[index++];
+        img.alt = options;
+    });        
+    }  */ 
